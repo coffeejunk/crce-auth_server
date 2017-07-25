f856c3f56a0138bbf697071ab5ea8462aa3e3b6d
Maximilian Haack <mxhaack@gmail.com>
2017-07-25 18:25:09 +0200

    Return UID for logged in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if ApiConsumer.exists?(email)
-      session[:user_id] = email
-      redirect_to sessions_current_path
-    else
-      ApiConsumer.create_from_omniauth(auth_hash)
-      session[:authenticated_email] = email
-      session[:user_id] = email
-      redirect_to sessions_current_path
-    end
+    consumer = if ApiConsumer.exists?(email)
+                 ApiConsumer.find_by email: email
+               else
+                 ApiConsumer.create_from_omniauth(auth_hash)
+               end
+    session[:user_id] = email
+    response.cookies['Auth-Token'] = consumer.uid # TODO: return actual token
+    redirect_to sessions_current_path
   end
 
   def destroy
@@ -23,7 +22,8 @@ class SessionsController < ApplicationController
 
   def current
     if session[:user_id].present?
-      render json: { email: session[:user_id] }
+      consumer = ApiConsumer.find_by email: session[:user_id]
+      render json: { email: consumer.email, uid: consumer.uid }
     else
       head :unauthorized
     end
